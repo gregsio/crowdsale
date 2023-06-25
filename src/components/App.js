407f3a9673104063aa159242b1c95b9d77d32bb0
gregs <github@gregs.io>
2023-06-25 12:52:26 +0200

    Adds provider to state

--- src/components/App.js
+++ src/components/App.js
@@ -9,10 +9,12 @@ import Info from "./Info";
 function App() {
 
     const [account, setAccount] = useState(null)
+    const [provider, setProvider] = useState(null)
 
     const loadBlockchainData = async () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum)
-        
+        setProvider(provider)
+
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
         const account = ethers.utils.getAddress(accounts[0])        
         setAccount(account)
